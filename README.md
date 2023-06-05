3b02797811d341652e39a23d2a3fca050b7a0dab
Matthew Croak <matthewc@socketmobile.com>
2023-06-05 10:17:15 -0400

    updated demo link and app credentials

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # SingleEntryJS
 
 SingleEntryJS is a very simple HTML and JS boilerplate app using the Socket Mobile JavaScript Capture Module. If you are looking for the React project example, the repo can be found [here](https://github.com/SocketMobile/single-entry-react).
- 
+
 It shows how to use the Socket Mobile CaptureJS SDK to receive the decoded data from the Socket Mobile devices into an input box.
 
 The connection state of the Socket Mobile device is shown in a status field at the top of the app.
 
 ## Requirements
+
 1. You will need your Socket Mobile developer ID as well as an app ID. Your developer ID can be found under your developer profile when you log into the [Socket Mobile Developer Portal](https://www.socketmobile.com/developers/portal). Then you need to [create your appKey](https://www.socketmobile.com/developers/portal/application-details/appkey-registration). For platform, select Web. For Language/Capture Client, select JavaScript. Your bundleId needs to be in the format of `socketmobile.com.yourappname`.
 2. The scanner needs to be paired with your devices in Application Mode. This can be done using the Socket Mobile Companion app (recommended), which can be downloaded from the [App Store](https://apps.apple.com/us/app/socket-mobile-companion/id1175638950). To pair your scanner in Application Mode without using Companion app, follow the instructions at: ConfigureInAppMode.
 
@@ -15,36 +16,16 @@ The connection state of the Socket Mobile device is shown in a status field at t
 You shouldn't need to install anything in order to use this boilerplate. However, if you want to temporarily host your app securely, you can install `ngrok` and follow the directions from this blog post (link to my blog post about using SimpleHTTPServer and ngrok).
 
 ## Usage
-To run web app on your laptop, connect an android device to your laptop so you can use `adb`. More in a moment. Then, use the Socket Mobile Companion app to connect your scanner to your attached device. Once you have successfully connected your scanner to your device, configure your credentials for your app (see below) and then in your terminal, in the project root run `adb forward tcp:18481 tcp:18481`. 
 
-The boilerplate HTML references the file `credentials.js`. The `.gitignore` file ignores this file as it is meant to act as an environment variable file, protecting your credentials from being exposed in a public GitHub repository. In this file is where you are to include your developerId, appKey and appId. See an example `credentials.js` file below.
+To run the web app on your laptop, connect an android device to the same machine so you can use `adb`. Then, use the Socket Mobile Companion app to connect your scanner to your attached device. Once you have successfully connected your scanner to your device, configure your credentials for your app (see below) and then in your project root, run `adb forward tcp:18481 tcp:18481` in the command line.
 
-```
-const CREDENTIALS = {
-    appId: "web:socketmobile.com.yourappname",
-    developerId: 'your-d3v-id',
-    appKey: 'alphanum3r1cappk3y'
-}
-```
-
-Then you can access those variables in your `index.js` like so...
-```
-const {appId, developerId, appKey} = CREDENTIALS;
-
-let appInfo = {
-    appId,
-    developerId,
-    appKey
-};
-```
-
-NOTE: This usage is optional and you can remove references to `credentials.js` and `CREDENTIALS` and use your credentials directly in your `index.js` file like so...
+Your generated appInfo should look something like the below...
 
 ```
 let appInfo = {
-    appId: "web:socketmobile.com.yourappname",
-    developerId: "your-d3v-id",
-    appKey: 'ALPHAnum3r1cAPPk3y'
+    appId: "web:socketmobile.com.singleentryjs",
+    developerId: '289d7d25-20ea-eb11-bacb-000d3a5b57ef',
+    appKey: 'MC4CFQDTXgyqNtrLf0MODRcyn2slp3K4/wIVAMe4pKxYfyqGbvVeqv46nqMcEC/P'
 };
 ```
 
@@ -89,7 +70,7 @@ const onCaptureEvent = (e, handle) => {
         ...
         break
         case CaptureEventIds.DeviceRemoval: //removing devices; scanner connects to companion/device
-        ... 
+        ...
         break
         case CaptureEventIds.DecodedData: //processing captured data; executed when you scan a code
         ...
@@ -101,3 +82,5 @@ const onCaptureEvent = (e, handle) => {
 We've provided some logic to handle adding devices, removing devices, scanning items and storing those scan results (and connected devices) and adding them to the DOM. Below is what your boilerplate should look like after connecting a scanner and scanning a few tags.
 
 ![javascript demo](./assets/js-demo.gif)
+
+You can check out the live JS demo [here](https://singleentry.socketmobile.dev/).
--- index.js
+++ index.js
@@ -1,186 +1,189 @@
-//Remove below line and add the values for each key directly 
-//if you do not want store them in credentials.js
-const {appId, developerId, appKey} = CREDENTIALS;
 let capture, devices;
 let decodedData = [];
-let deviceMap = {}
+let deviceMap = {};
 let dataId = 10;
 
 let appInfo = {
-    appId,
-    developerId,
-    appKey
+  appId: "web:socketmobile.com.singleentryjs",
+  developerId: "289d7d25-20ea-eb11-bacb-000d3a5b57ef",
+  appKey: "MC4CFQDTXgyqNtrLf0MODRcyn2slp3K4/wIVAMe4pKxYfyqGbvVeqv46nqMcEC/P",
 };
 
 const onCaptureEvent = (e, handle) => {
-    const {CaptureEventIds, Capture} = SocketMobile
-    if (!e) {
+  const { CaptureEventIds, Capture } = SocketMobile;
+  if (!e) {
+    return;
+  }
+  switch (e.id) {
+    // **********************************
+    // Device Arrival Event
+    //   a device needs to be opened in
+    //   to receive the decoded data
+    //  e = {
+    //    id: CaptureEventIds.DeviceArrival,
+    //    type: CaptureEventTypes.DeviceInfo,
+    //    value: {
+    //      guid: "b876d9a8-85b6-1bb5-f1f6-1bb5d78a2c6e",
+    //      name: "Socket S740 [E2ABB4]",
+    //      type: CaptureDeviceType.ScannerS740
+    //    }
+    //  }
+    // **********************************
+    case CaptureEventIds.DeviceArrival:
+      const newDevice = new Capture();
+      const { guid, name } = e.value;
+      newDevice
+        .openDevice(guid, capture)
+        .then((result) => {
+          updateStatus(`result of opening ${name} : ${result}`);
+          devices = devices || [];
+          if (!deviceMap[name]) {
+            devices.push({
+              guid,
+              name,
+              handle: newDevice.clientOrDeviceHandle,
+              device: newDevice,
+            });
+            deviceMap[name] = true;
+          }
+          updateDevices();
+        })
+        .catch((err) => {
+          updateStatus(`error opening a device: ${err}`);
+        });
+      break;
+    // **********************************
+    // Device Removal Event
+    //   it is better to close the device
+    //  e = {
+    //    id: CaptureEventIds.DeviceRemoval,
+    //    type: CaptureEventTypes.DeviceInfo,
+    //    value: {
+    //      guid: "b876d9a8-85b6-1bb5-f1f6-1bb5d78a2c6e",
+    //      name: "Socket S740 [E2ABB4]",
+    //      type: CaptureDeviceType.ScannerS740
+    //    }
+    //  }
+    // **********************************
+    case CaptureEventIds.DeviceRemoval:
+      const removeDevice = devices.find((d) => d.guid === e.value.guid);
+      if (!removeDevice) {
+        updateStatus(`no matching devices found for ${e.value.name}`);
         return;
-    }
-    switch (e.id) {
-        // **********************************
-        // Device Arrival Event
-        //   a device needs to be opened in
-        //   to receive the decoded data
-        //  e = {
-        //    id: CaptureEventIds.DeviceArrival,
-        //    type: CaptureEventTypes.DeviceInfo,
-        //    value: {
-        //      guid: "b876d9a8-85b6-1bb5-f1f6-1bb5d78a2c6e",
-        //      name: "Socket S740 [E2ABB4]",
-        //      type: CaptureDeviceType.ScannerS740
-        //    }
-        //  }
-        // **********************************
-        case CaptureEventIds.DeviceArrival:
-            const newDevice = new Capture();
-            const {guid, name} = e.value;
-            newDevice
-                .openDevice(guid, capture)
-                .then(result => {
-                    updateStatus(`result of opening ${name} : ${result}`);
-                    devices = devices || []
-                    if (!deviceMap[name]){
-                        devices.push({
-                            guid,
-                            name,
-                            handle: newDevice.clientOrDeviceHandle,
-                            device: newDevice,
-                        });
-                        deviceMap[name] = true
-                    }
-                    updateDevices()
-                })
-                .catch(err => {
-                    updateStatus(`error opening a device: ${err}`);
-                });
-            break;
-        // **********************************
-        // Device Removal Event
-        //   it is better to close the device
-        //  e = {
-        //    id: CaptureEventIds.DeviceRemoval,
-        //    type: CaptureEventTypes.DeviceInfo,
-        //    value: {
-        //      guid: "b876d9a8-85b6-1bb5-f1f6-1bb5d78a2c6e",
-        //      name: "Socket S740 [E2ABB4]",
-        //      type: CaptureDeviceType.ScannerS740
-        //    }
-        //  }
-        // **********************************
-        case CaptureEventIds.DeviceRemoval:
-            const removeDevice = devices.find(d => d.guid === e.value.guid);
-            if (!removeDevice) {
-                updateStatus(`no matching devices found for ${e.value.name}`)
-                return;
-            }
-            removeDevice.device
-                .close()
-                .then(result => {
-                    updateStatus(`result of closing ${removeDevice.name}: ${result}`);
-                    devices = devices.filter(x=>x.guid !== removeDevice.guid)
-                    updateDevices()
-                })
-                .catch(err => {
-                    updateStatus(`error closing a device: ${err}`, err);
-                });
-          break;
-        // **********************************
-        // Decoded Data
-        //   receive the decoded data from
-        //   a specific device
-        //  e = {
-        //    id: CaptureEventIds.DecodedData,
-        //    type: CaptureEventTypes.DecodedData,
-        //    value: {
-        //      data: [55, 97, 100, 57, 53, 100, 97, 98, 48, 102, 102, 99, 52, 53, 57, 48, 97, 52, 57, 54, 49, 97, 51, 49, 57, 50, 99, 49, 102, 51, 53, 55],
-        //      id: CaptureDataSourceID.SymbologyQRCode,
-        //      name: "QR Code"
-        //    }
-        //  }
-        // **********************************
-        case CaptureEventIds.DecodedData:
-            const deviceSource = devices.find(d => d.handle === handle);
-            
-            if (!deviceSource) {
-                updateStatus(`no matching devices found for ${e.value.name}`)
-            }
-            decodedData.push({id: dataId, name: e.value.name, data: String.fromCharCode.apply(null, e.value.data)})      
-            updateData()
-          break;
       }
-}
+      removeDevice.device
+        .close()
+        .then((result) => {
+          updateStatus(`result of closing ${removeDevice.name}: ${result}`);
+          devices = devices.filter((x) => x.guid !== removeDevice.guid);
+          updateDevices();
+        })
+        .catch((err) => {
+          updateStatus(`error closing a device: ${err}`, err);
+        });
+      break;
+    // **********************************
+    // Decoded Data
+    //   receive the decoded data from
+    //   a specific device
+    //  e = {
+    //    id: CaptureEventIds.DecodedData,
+    //    type: CaptureEventTypes.DecodedData,
+    //    value: {
+    //      data: [55, 97, 100, 57, 53, 100, 97, 98, 48, 102, 102, 99, 52, 53, 57, 48, 97, 52, 57, 54, 49, 97, 51, 49, 57, 50, 99, 49, 102, 51, 53, 55],
+    //      id: CaptureDataSourceID.SymbologyQRCode,
+    //      name: "QR Code"
+    //    }
+    //  }
+    // **********************************
+    case CaptureEventIds.DecodedData:
+      const deviceSource = devices.find((d) => d.handle === handle);
+
+      if (!deviceSource) {
+        updateStatus(`no matching devices found for ${e.value.name}`);
+      }
+      decodedData.push({
+        id: dataId,
+        name: e.value.name,
+        data: String.fromCharCode.apply(null, e.value.data),
+      });
+      updateData();
+      break;
+  }
+};
 
 //to ensure the cdn is loaded before you try to use prefix
-window.addEventListener('DOMContentLoaded', ()=>{
+window.addEventListener("DOMContentLoaded", () => {
+  capture = new SocketMobile.Capture();
 
-    capture = new SocketMobile.Capture();
+  capture
+    .open(appInfo, onCaptureEvent)
+    .then((result) => {
+      console.log("opening Capture result: ", result);
+      updateStatus(`opening Capture result: ${result}`);
+    })
+    .catch((err) => {
+      var finalErr = err.error || err;
+      var val;
+      // error code to watch for to check if the Companion service is running
+      if (finalErr === SocketMobile.SktErrors.ESKT_UNABLEOPENDEVICE) {
+        val =
+          "not able to connect to the service, is it running?" + ` ${finalErr}`;
+        console.log("no able to connect to the service, is it running?");
+      } else {
+        val = `opening Capture error: ${finalErr}`;
+      }
+      console.log(val);
+      updateStatus(val, finalErr);
+    });
+});
 
-    capture.open(appInfo, onCaptureEvent)
-        .then(result => {
-            console.log('opening Capture result: ', result);
-            updateStatus(`opening Capture result: ${result}`)
-        })
-        .catch(err => {
-            var finalErr = err.error || err
-            var val;
-            // error code to watch for to check if the Companion service is running
-            if(finalErr === SocketMobile.SktErrors.ESKT_UNABLEOPENDEVICE){
-                val='not able to connect to the service, is it running?' + ` ${finalErr}`
-                console.log('no able to connect to the service, is it running?');
-            }
-            else {
-                val = `opening Capture error: ${finalErr}`;
-            }
-            console.log(val)
-            updateStatus(val, finalErr)
-        });
-})
+const updateStatus = (val, err) => {
+  var color = err ? "red" : "green";
+  document.getElementById("status").innerText = val;
+  document.getElementById("status").style.color = color;
+};
 
-const updateStatus = (val, err) =>{
-    var color = err ? 'red' : 'green'
-    document.getElementById('status').innerText = val
-    document.getElementById('status').style.color = color
-}
-
-const updateDevices = () =>{
-   var table = document.getElementById('devices')
-   table.children[0].innerHTML = `<tr>
+const updateDevices = () => {
+  var table = document.getElementById("devices");
+  table.children[0].innerHTML = `<tr>
         <th>Name</th>
         <th>GUID</th>
-    </tr>`
-   for (var i = 0; i < devices.length; ++i){
-       var row = table.insertRow(i+1);
-       var cell1 = row.insertCell(0);
-       var cell2 = row.insertCell(1);
-       cell1.innerText = devices[i].name
-       cell2.innerText = devices[i].guid
-   }
-   if (devices.length > 0){
-       document.getElementById("device-warning").style.visibility = 'hidden'
-   } else {
-       document.getElementById("device-warning").style.visibility = 'visible'
-   }
-}
+    </tr>`;
+  for (var i = 0; i < devices.length; ++i) {
+    var row = table.insertRow(i + 1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerText = devices[i].name;
+    cell2.innerText = devices[i].guid;
+  }
+  if (devices.length > 0) {
+    document.getElementById("device-warning").style.visibility = "hidden";
+  } else {
+    document.getElementById("device-warning").style.visibility = "visible";
+  }
+};
 
 const onClear = () => {
-    dataId = 10
-    decodedData = []
-    document.getElementById('data-list').innerHTML = ""
-    document.getElementById('decoded-data').value = ""
-}
+  dataId = 10;
+  decodedData = [];
+  document.getElementById("data-list").innerHTML = "";
+  document.getElementById("decoded-data").value = "";
+};
 
 const updateData = () => {
-    dataId++
-    var list = document.getElementById('data-list')
-    list.innerHTML = ""
-    for (var i = 0; i < decodedData.length; ++i){
-        var l = document.createElement("LI")
-        l.innerText = `${decodedData[i].name.toUpperCase()} (${decodedData[i].data.length}): ${decodedData[i].data}`
-        list.appendChild(l)
-        if (i === decodedData.length - 1){
-            //assign scanned value to input
-            document.getElementById('decoded-data').value = l.innerText
-        }
+  dataId++;
+  var list = document.getElementById("data-list");
+  list.innerHTML = "";
+  for (var i = 0; i < decodedData.length; ++i) {
+    var l = document.createElement("LI");
+    l.innerText = `${decodedData[i].name.toUpperCase()} (${
+      decodedData[i].data.length
+    }): ${decodedData[i].data}`;
+    list.appendChild(l);
+    if (i === decodedData.length - 1) {
+      //assign scanned value to input
+      document.getElementById("decoded-data").value = l.innerText;
     }
-}
+  }
+};
